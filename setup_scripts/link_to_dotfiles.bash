f2faac99e0a72db5f951e590dd4436803ad7f3fa
akira hamada <drumcorps.enthusiast@gmail.com>
2014-01-26 00:32:30 +0900

    add .gitconfig and .unite/

--- setup_scripts/link_to_dotfiles.bash
+++ setup_scripts/link_to_dotfiles.bash
@@ -1,5 +1,7 @@
 ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.unite ~/.unite
 ln -s ~/dotfiles/.zsh_profile ~/.zsh_profile
 ln -s ~/dotfiles/.zshrc ~/.zshrc
 ln -s ~/dotfiles/.bash_profile ~/.bash_profile
 ln -s ~/dotfiles/.bashrc ~/.bashrc
+ln -s ~/dotfiles/.gitconfig ~/.gitconfig
